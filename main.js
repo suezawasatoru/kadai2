dac2a2efccea45381cd36aa6783343d6313f42ea
末澤悟 <suesawasatoru@suesawasatorunoMacBook-Air.local>
2023-09-08 11:33:25 +0900

    課題の再提出

---
+++ main.js
@@ -0,0 +1,54 @@
+let timer;
+let elapsedTime = 0;
+
+function updateDisplay() {
+  let milliseconds = elapsedTime % 1000;
+  let seconds = Math.floor((elapsedTime / 1000) % 60);
+  let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  let hour = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+  $('#milliseconds').text(milliseconds.toString().padStart(3, '0'));
+  $('#seconds').text(seconds.toString().padStart(2, '0'));
+  $('#minutes').text(minutes.toString().padStart(2, '0'));
+  $('#hour').text(hour.toString().padStart(2, '0'));
+}
+
+$(document).ready(function () {
+  $('#start').on('click', function () {
+    if (!timer) {
+      timer = setInterval(function () {
+        elapsedTime += 10;
+        updateDisplay()
+      }, 10);
+      $('#start').prop('disabled', true);
+      $('#stop').prop('disabled', false);
+      $('#reset').prop('disabled', false);
+    };
+  });
+  $('#stop').on('click', function () {
+    if (timer) {
+      clearInterval(timer);
+      timer = null;
+
+      $('#start').prop('disabled', false);
+      $('#stop').prop('disabled', true);
+      $('#reset').prop('disabled', false);
+    };
+  });
+  $('#reset').on('click', function () {
+    if (!timer) {
+      elapsedTime = 0;
+      updateDisplay();
+      $('#reset').prop('disabled', true);
+    };
+
+    if (timer) {
+      elapsedTime = 0;
+      updateDisplay();
+      $('#reset').prop('disabled', false);
+
+    };
+  });
+});
+
+
